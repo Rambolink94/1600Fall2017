aa7f6eabb29969646035676d8d21ebbb87b4ec08
Knight Steele <knightsteele94@gmail.com>
2017-08-28 12:00:56 -0600

    Added dying animals scripts.

---
+++ Project 1600 Fall 2017/Assets/Scripts/Fish.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : Animal {
+
+	void Swim(){
+		print (this.name + " Swims");
+	}
+
+	void Start(){
+		Swim ();
+	}
+}
