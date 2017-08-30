fc8f5a98658438101649384df42e2d16bd6066d1
Knight Steele <knightsteele94@gmail.com>
2017-08-30 11:38:07 -0600

    Added class diversity and subclass simuarity.

--- Project 1600 Fall 2017/Assets/Scripts/Mammal.cs
+++ Project 1600 Fall 2017/Assets/Scripts/Mammal.cs
@@ -3,5 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Mammal : Animal {
+	public override void Start(){
+		base.Start ();
+		Shed ();
+	}
+	void Shed(){
+		print (this.name + " Shed");
+	}
 
 }
