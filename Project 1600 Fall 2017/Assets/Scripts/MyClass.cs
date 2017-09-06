04bb9b36fac309ec2fdba62bfec5e099ce0a1033
Knight Steele <knightsteele94@gmail.com>
2017-09-06 11:52:01 -0600

    Deleted Animal Scripts, added myClass Script.

---
+++ Project 1600 Fall 2017/Assets/Scripts/MyClass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyClass : MonoBehaviour {
+
+	string myString = "Hello";
+	string myBottle = "My Bottle";
+	int myInt = 5;
+	char myChar = 'H';
+	// Use this for initialization
+	void Start () {
+		print(myString);
+		print(myBottle);
+		print(myInt);
+		print(myChar);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
