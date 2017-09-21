e17645420146d38199333c910d8f5912e8ec3e9c
Knight Steele <knightsteele94@gmail.com>
2017-09-20 23:41:06 -0600

    Added both an Array and While loop script.

---
+++ Homework Scripts/Homework Project/Assets/Scripts/Array.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Array : MonoBehaviour {
+
+    public string[] powerUp;
+
+    void Start()
+    {
+        foreach (string i in powerUp) {
+            print(i);
+        }
+    }
+}
---
+++ Homework Scripts/Homework Project/Assets/Scripts/While.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class While : MonoBehaviour {
+
+    public int Health = 250;
+    public int Armor = 100;
+    public int Shield = 50;
+
+    private bool isAlive = true;
+
+	// Use this for initialization
+	void Start () {
+        while (Health > 0 && Armor > 0 && Shield > 0) {
+            print("Player is Alive");
+            return;
+        }
+        if (isAlive == false) {
+            print("Player is Dead");
+        }
+	}
+
+    public void decreaseHealth() {
+        Health -= 10;
+        print(Health);
+    }
+
+    public void decreaseArmor() {
+        Armor -= 10;
+        print(Armor);
+    }
+
+    public void decreaseShield() {
+        Shield -= 10;
+        print(Shield);
+    }
+}
