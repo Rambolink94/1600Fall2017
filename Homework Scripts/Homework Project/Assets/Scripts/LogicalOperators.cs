f113dde0c4466bf56d7f3365245ef3ea154a23c9
Knight Steele <knightsteele94@gmail.com>
2017-09-25 13:53:38 -0600

    Added Logic Operators Script to Homework project.
    Script desides various conditions in which you can become king....or not.

---
+++ Homework Scripts/Homework Project/Assets/Scripts/LogicalOperators.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicalOperators : MonoBehaviour {
+
+    public int age;
+    public int support;
+    public int morale;
+    public bool heir;
+    public bool usurper;
+    public bool raisedLevies;
+    public bool male;
+    public bool purpose;
+    public bool fatherWasKing;
+    public bool youOldestSon;
+
+    void Update()
+    {
+        if ((male == true || usurper == true) && age >= 16 && (heir == true || usurper == true))
+        {
+            print("You are worthy to become king");
+        }
+        else {
+            print("You are not worthy to become king");
+        }
+        if (raisedLevies == true && (support >= 1000 || morale >= 65) && purpose == true)
+        {
+            print("You have the power to usurp");
+        }
+        else {
+            print("You do not yet have the power to usurp");
+        }
+        if (fatherWasKing == true && youOldestSon == true)
+        {
+            print("You are the rightfull heir to the throne");
+        }
+        else {
+            print("You are not the rightfull heir to the throne");
+        }
+    }
+}
