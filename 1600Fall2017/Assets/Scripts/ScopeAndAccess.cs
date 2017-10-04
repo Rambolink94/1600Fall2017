90a21caa55eb3732fc15beaed17816fc928985af
Knight Steele <knightsteele94@gmail.com>
2017-10-04 11:53:57 -0600

    Added Scope and Access Modifiers.

---
+++ 1600Fall2017/Assets/Scripts/ScopeAndAccess.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScopeAndAccess : MonoBehaviour {
+
+	private int health = 100;
+
+	public int score = 1000;
+
+	string thingOne = "Thing One";
+
+	void Box() {
+		int newScore = 100;
+		thingOne = "Fun";
+		print (newScore);
+		print (thingOne);	
+
+		string thingTwo = "Thing Two";
+		print (thingTwo);
+	}
+
+}
