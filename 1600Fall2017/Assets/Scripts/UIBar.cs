5d8f1ff8b891356e1a6100584dcaf9a7e4e18a18
Knight Steele <knightsteele94@gmail.com>
2017-10-25 11:54:44 -0600

    Added Powerup and Powerdown.

--- 1600Fall2017/Assets/Scripts/UIBar.cs
+++ 1600Fall2017/Assets/Scripts/UIBar.cs
@@ -7,16 +7,46 @@ public class UIBar : MonoBehaviour {
 
 	public Image bar;
 	public float barTime = 0.1f;
+	public float powerLevel = 0.1f;
+	public float amountToAdd = 0.01f;
+
+	public enum PowerUpType
+	{
+		PowerUP,
+		PowerDown
+	}
+
+	public PowerUpType powerUp;
 
 	// Use this for initialization
-	void Start () {
-		StartCoroutine (UpdateBar ());
+	void OnTriggerEnter () {
+		switch (powerUp)
+		{
+		case PowerUpType.PowerUP:
+			StartCoroutine(PowerUpBar());
+			break;
+		case PowerUpType.PowerDown:
+			StartCoroutine (PowerDownBar());
+			break;
+		}
 	}
 	
-	IEnumerator UpdateBar () {
+	IEnumerator PowerUpBar () {
 		while(bar.fillAmount < 1) 
 		{
-			bar.fillAmount += barTime;
+			bar.fillAmount += amountToAdd;
+			yield return new WaitForSeconds (barTime);
+		}
+	}
+
+	IEnumerator PowerDownBar () {
+		float tempAmount = powerLevel;
+		float fillAmount = bar.fillAmount;
+		while (powerLevel > 0) 
+		{
+			fillAmount = tempAmount - amountToAdd;
+			bar.fillAmount = fillAmount;
+
 			yield return new WaitForSeconds (barTime);
 		}
 	}
