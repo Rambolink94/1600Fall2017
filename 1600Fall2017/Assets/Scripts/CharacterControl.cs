e783e7ec14c85146135f89a6184a5574c467110c
Knight Steele <knightsteele94@gmail.com>
2017-10-30 17:25:49 -0600

    Created movement script for Beni.

--- 1600Fall2017/Assets/Scripts/CharacterControl.cs
+++ 1600Fall2017/Assets/Scripts/CharacterControl.cs
@@ -26,7 +26,8 @@ public class CharacterControl : MonoBehaviour {
 			{
 				moveVector3.y = jumpForce * Time.deltaTime;
 			}
-		}
+            movement.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        }
 		moveVector3.x = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
 		characterController.Move (moveVector3);
 	}
--- The Mummy, The Adventures of Beni/Assets/Scripts/PlayerController.cs
+++ The Mummy, The Adventures of Beni/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public CharacterController characterController;
+
+    public float speed = 10;
+    public float gravity = 3f;
+    public float jumpForce = 20;
+
+    public Vector3 movement;
+
+    void Update()
+    {
+        movement.y -= gravity * Time.deltaTime;
+
+        if (characterController.isGrounded)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                movement.y += jumpForce * Time.deltaTime;
+            }
+            movement.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        }
+        movement.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        characterController.Move(movement);
+    }
 }
