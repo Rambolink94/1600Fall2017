1a9ca367a43a37c1d6f433025e23e8b9a9c44140
Knight Steele <knightsteele94@gmail.com>
2017-12-06 11:49:19 -0700

    Added more level stuff, and started destroying picked up powerups.

---
+++ The Mummy, The Adventures of Beni/Assets/Scripts/PowerUpCooldown.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpCooldown : MonoBehaviour {
+
+    public float powerUpCooldown = 10;
+
+    void OnCollisionEnter2D()
+    {
+        StartCoroutine (Cooldown);
+    }
+
+    IEnumerator Cooldown() {
+        
+    }
+}
